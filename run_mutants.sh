5769cddf7600f70121ba2f625297221b7e7f71f5
Maddie Jones <madeleine.jones@hotmail.com>
2017-03-08 21:40:00 +0000

    Using a nicer regex in the grep in the run script

--- run_mutants.sh
+++ run_mutants.sh
@@ -5,6 +5,6 @@ do
 
 	echo Testing m_$A.jar
 	javac -cp .:/usr/share/java/junit4.jar:./Mutations/m_$A.jar Task22.java
-	java -cp .:/usr/share/java/junit4.jar:./Mutations/m_$A.jar org.junit.runner.JUnitCore Task22 | egrep "\.{3}(\.+E?)+"
+	java -cp .:/usr/share/java/junit4.jar:./Mutations/m_$A.jar org.junit.runner.JUnitCore Task22 | egrep "(\.|E){20}"
 
 done
